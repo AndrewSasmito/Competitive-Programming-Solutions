5aa91f33450cecea1eda925ac5382ec81e347b5c
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-05-30 22:14:49 -0400

    Create UCC Coding Competition '21 P3 - Long Pizza.cpp

---
+++ UCC Coding Competition/UCC Coding Competition '21 P3 - Long Pizza.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, x, y, q, l, r;
+ll sum = 0;
+void solve(){
+	cin >> n >> x >> y >> q;
+
+	for (int i = 0; i<q; ++i){
+		cin >> l >> r;
+		
+		sum += max(0, min(y, r) - max(l, x) + 1);
+	}
+	cout << sum << '\n';
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int testcases = 1;
+    //cin >> testcases;
+    for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+}
