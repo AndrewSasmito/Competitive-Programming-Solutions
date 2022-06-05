9406bcb17a76955ae43a5ef45d693fdd8eb19059
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-05 17:12:26 -0400

    Create TLE '17 Contest 2 P2 - Unlucky Numbers.cpp

---
+++ TLE Contest/TLE '17 Contest 2 P2 - Unlucky Numbers.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, k, x;
+vector <int> v;
+void solve(){
+	cin >> n;
+
+	for (int i = 0; i<n; ++i){
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+
+	cin >> k;
+	for (int i = 0; i <k; ++i){
+		cin >> x;
+		
+		auto it = upper_bound(v.begin(), v.end(), x);
+
+		cout << x - (it - v.begin()) << '\n';
+	}
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
