609c8a13857f6e3d621c54ab4246fba6638cfcbd
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-05 22:32:42 -0400

    Create Hardcore Grinding.cpp

---
+++ Uncategorized - Dmoj/Hardcore Grinding.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n;
+ll x, y;
+ll dp[10000001];
+void solve(){
+	cin >> n;
+	
+	for (int i = 0; i<n; ++i){
+		cin >> x >> y;
+		++dp[x];
+		--dp[y];
+	}
+	
+	int mx = 0, cnt = 0;
+
+	for (int i = 1; i<=10000000; ++i){
+		cnt += dp[i];
+		mx = max(mx, cnt);
+	}
+	
+	cout << mx << '\n';
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
