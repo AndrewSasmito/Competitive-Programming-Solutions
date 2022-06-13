a95dc258e405be5304bfe925c601e28b7bb2dbd4
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-12 20:03:29 -0400

    Create Popular Bread.cpp

---
+++ Miscellaneous/Popular Bread.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, m, ans = 0, x, y, mx = -1, mn = 1000000;
+void solve(){
+	cin >> n >> m;
+
+	for (int i = 0; i<m; ++i){
+		cin >> x >> y;
+		
+		mx = max(x, mx);
+		mn = min(y, mn);
+	}
+
+	cout << max(0, mn - mx + 1) << '\n';
+}
+
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
