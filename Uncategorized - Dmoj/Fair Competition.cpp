a466b946fb38aff1592b830a62a26ec715ce5380
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-19 18:20:15 -0400

    Create Fair Competition.cpp

---
+++ Uncategorized - Dmoj/Fair Competition.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, m, x;
+unordered_map <int, int> mp;
+set <int> s;
+void solve(){
+	cin >> m >> n;
+
+	for (int i = 0; i<m; ++i){
+		cin >> x;
+		++mp[x];
+		s.insert(x);
+	}
+
+	for (int x : s){
+		if (m <= n){
+			cout << m << '\n';
+			return;
+		}else{
+			m -= mp[x];
+		}
+	}
+	cout << m << '\n';
+}
+
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
