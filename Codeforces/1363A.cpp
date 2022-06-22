008021e252173a4ba6f6b7f032d2be41e5f9bb8b
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-21 23:03:28 -0400

    Create 1363A.cpp

---
+++ Codeforces/1363A.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, x, a, odd, eve;
+void solve(){
+	cin >> n >> x;
+
+	odd = 0;
+	eve = 0;
+
+	for (int i = 0; i<n; ++i){
+		cin >> a;
+
+		odd += (a % 2 == 1);
+		eve += (a % 2 == 0);
+	}
+
+	if (odd == 0){
+		cout << "No\n";
+		return;
+	}
+
+	--x;
+	--odd;
+
+	if (x % 2 == 1){
+		if (eve == 0){
+			cout << "No\n";
+			return;
+		}
+
+		--eve;
+		--x;
+	}
+
+	x -= (odd / 2) * 2;
+	x -= eve;
+
+	if ( x <= 0){
+		cout << "Yes\n";
+		return;
+	}else{
+		cout << "No\n";
+	}
+}
+
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
