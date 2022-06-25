89de5f136a11057275bf585c7aafe8616d88a09b
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-25 17:29:18 -0400

    Create 832A.cpp

---
+++ Codeforces/832A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+ll n, k;
+void solve(){
+	cin >> n >> k;
+
+	cout << ((n/k) % 2 == 0 ? "NO" : "YES") << '\n';
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
