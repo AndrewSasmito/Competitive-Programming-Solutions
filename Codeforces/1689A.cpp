8d29b7e6e1cb756906f703adeb3d64dd1a4d60e0
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-27 20:50:05 -0400

    Create 1689A.cpp

---
+++ Codeforces/1689A.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, m, k;
+string a, b, c;
+void solve(){
+	cin >> n >> m >> k;
+
+	cin >> a >> b;
+	c = "";
+	sort (a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	int i = 0, j = 0, cnt = 0, cur = 0;
+
+	while (true){
+		
+		if (i < n && j < m){
+			if (cnt == k){
+				if (cur == 0){
+					c += b[j];
+					++j;
+					cnt = 1;
+					cur = 1;
+				}else{
+					c += a[i];
+					++i;
+					cnt = 1;
+					cur = 0;
+				}
+			}else if (a[i] < b[j]){
+				c += a[i];
+				++i;
+
+				if (cur == 1){
+					cnt = 1;
+					cur = 0;
+				}else{
+					++cnt;
+				}
+			}else if (b[j] < a[i]){
+				c += b[j];
+				++j;
+
+				if (cur == 0){
+					cnt = 1;
+					cur = 1;
+				}else{
+					++cnt;
+				}
+			}
+		}else{
+			break;
+		}
+	}
+	cout << c << '\n';
+
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
