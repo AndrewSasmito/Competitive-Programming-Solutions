b5739bc767a01cdb8c21ee62109a9504a4b7d22f
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-06-28 20:56:48 -0400

    Create 1237A.cpp

---
+++ Codeforces/1237A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int n, pcnt = 1, x;
+void solve(){
+	cin >> n;
+
+	for (int i = 0; i<n; ++i){
+		cin >> x;
+
+		if (x % 2 == 0){
+			cout << x / 2 << '\n';
+		}else{
+			cout << (x + pcnt) / 2 << '\n';
+			pcnt *= -1;
+		}
+	}
+
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
