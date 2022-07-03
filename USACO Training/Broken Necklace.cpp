8ac238c8e946df15b696a68bde3a89de338b0734
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-07-02 22:41:35 -0400

    Create Broken Necklace.cpp

---
+++ USACO Training/Broken Necklace.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedTime ((double)(clock()-__init_time__)/CLOCKS_PER_SEC)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+
+//Custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+//unordered_map<long long, int, custom_hash> safe_map;
+//gp_hash_table<long long, int, custom_hash> safe_hash_table;
+
+void usaco(){
+	freopen("beads.in", "r", stdin);
+	freopen("beads.out", "w", stdout);
+}
+
+//do not submit if usaco(); is open
+int n, ans = 0;
+string s;
+void solve(){
+	usaco();
+	cin >> n >> s;
+
+	s += s;
+	
+	bool red = 1;
+	int rcnt = 0, bcnt = 0, wcnt = 0;
+
+	if (s[0] == 'w'){
+		++wcnt;
+		++rcnt;
+	}else if (s[0] == 'r'){
+		++rcnt;
+	}else{
+		++bcnt;
+		red = 0;
+	}
+
+	for (int i = 1; i<s.size(); ++i){
+		if (s[i] == 'r'){
+			if (red == 1){
+				++rcnt;
+				wcnt = 0;
+			}else{
+				ans = max(ans, rcnt + bcnt);
+
+				rcnt = wcnt + 1;
+				bcnt -= wcnt;
+				wcnt = 0;
+				red = 1;
+			}
+		}else if (s[i] == 'b'){
+			if (red == 0){
+				++bcnt;
+				wcnt = 0;
+			}else{
+				ans = max(ans, rcnt + bcnt);
+				bcnt = wcnt + 1;
+				rcnt -= wcnt;
+				wcnt = 0;
+				red = 0;
+			}
+		}else{
+			++wcnt;
+			if (red == 0){
+				++bcnt;
+			}else{
+				++rcnt;
+			}
+		}
+	}
+
+	ans = max(ans, rcnt + bcnt);
+
+	cout << min(ans, n) << '\n';
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
