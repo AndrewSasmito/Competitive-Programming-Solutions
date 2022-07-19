e145f61d15682b954b435a6ff81d599eaaf4e0dc
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-07-18 23:03:50 -0400

    Create 1699B.cpp

---
+++ Codeforces/1699B.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+};
+
+//unordered_map<long long, int, custom_hash> safe_map;
+//gp_hash_table<long long, int, custom_hash> safe_hash_table;
+
+void usaco(){
+	freopen("wormhole.in", "r", stdin);
+	freopen("wormhole.out", "w", stdout);
+}
+
+int n, m;
+bool dp[51][51], f, z;
+void solve(){
+	cin >> n >> m;
+	
+	f = 1;
+	z = 0;
+
+		for (int j = 0; j < m; j += 2){
+			if (f == 1){
+				dp[0][j] = 1;
+				dp[0][j+1] = 0;
+			}else{
+				dp[0][j] = 0;
+				dp[0][j+1] = 1;
+			}
+			f = 1 - f;
+		}
+
+
+	for (int i = 1; i<n; ++i){
+		for (int j = 0; j<m; ++j){
+			if (i % 2 == 1){
+				dp[i][j] = 1 - dp[i-1][j];
+			}else{
+				dp[i][j] = dp[i-1][j];
+			}
+		}
+	}
+
+	for (int i = 0; i<n; ++i){
+		for (int j = 0; j<m; ++j){
+			cout << dp[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
+//do not submit if usaco(); is open
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
