3adde92d9a4782b17472af642b17361c87fc7d19
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-07-22 21:23:38 -0400

    Create 1702C.cpp

---
+++ Codeforces/1702C.cpp
@@ -0,0 +1,92 @@
+/*
+ID: asasmit1
+TASK: wormhole
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+};
+
+//unordered_map<long long, int, custom_hash> safe_map;
+//gp_hash_table<long long, int, custom_hash> safe_hash_table;
+
+void usaco(){
+	freopen("wormhole.in", "r", stdin);
+	freopen("wormhole.out", "w", stdout);
+}
+
+int n, k;
+ll x, y;
+void solve(){
+	unordered_map <ll, int, custom_hash> f1;
+	unordered_map <ll, int, custom_hash> f2;
+	cin >> n >> k;
+
+	for (int i = 1; i<=n; ++i){
+		cin >> x;
+		if (f1[x] == 0){
+			f1[x] = i;
+		}
+		f2[x] = i;
+	}
+	
+	for (int i = 0; i<k; ++i){
+		cin >> x >> y;
+
+		if (f1[x] < f2[y] && f1[x] > 0 && f2[y] > 0){
+			cout << "YES\n";
+		}else{
+			cout << "NO\n";
+		}
+	}
+}
+//do not submit if usaco(); is open
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
