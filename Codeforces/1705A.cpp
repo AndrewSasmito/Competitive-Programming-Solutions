212d9b7fb4de1ad40b02230fbff071006b8b718c
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-07-29 20:58:09 -0400

    Create 1705A.cpp

---
+++ Codeforces/1705A.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+};
+
+//unordered_map<long long, int, custom_hash> safe_map;
+//gp_hash_table<long long, int, custom_hash> safe_hash_table;
+
+void usaco(){
+	freopen("ariprog.in", "r", stdin);
+	freopen("ariprog.out", "w", stdout);
+}
+
+
+int n, x, a;
+void solve(){
+
+	cin >> n >> x;
+
+	vector <int> v;
+	for (int i = 0; i<n * 2; ++i){
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i<n; ++i){
+		if (v[i] + x > v[n + i]){
+			cout << "NO\n";
+			return;
+		}
+	}
+	cout << "YES\n";
+}
+
+//do not submit if usaco(); is open
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+	cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+
+
+    //Google
+    //cout<<"Case #"<<number_of_total_test_cases<<": ";
+    // Normal
+    solve();
+    }
+
+	//cout << elapsedTime << '\n';
+}
