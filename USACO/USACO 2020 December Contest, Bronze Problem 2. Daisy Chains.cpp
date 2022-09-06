7437590ca3702a6faca90ebcfd2bf552fe5505fb
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-09-06 19:15:13 -0400

    Create  USACO 2020 December Contest, Bronze Problem 2. Daisy Chains.cpp

---
+++ USACO/USACO 2020 December Contest, Bronze Problem 2. Daisy Chains.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+
+	/*
+	 	* How To Use:
+	 	*
+	 	* unordered_map<long long, int, custom_hash> safe_map;
+	 	* gp_hash_table<long long, int, custom_hash> safe_hash_table;
+	 	*
+	 */
+};
+
+
+
+void usaco(){
+
+	freopen("measurement.in", "r", stdin);
+	freopen("measurement.out", "w", stdout);
+
+	/*
+		 * How To Use:
+		 *
+		 * Call usaco() to take input and output
+		 * to & from a textfile
+		 *
+		 * freopen("File Name", "r" or "w", stdin or stdout);
+		 *
+		 * r && stdin == read
+		 *
+		 * w && stdout == print
+		 *
+	 */
+}
+
+int n, x, psa[101], cnt[101], ans, av;
+void solve(){
+	cin >> n;
+
+	ans = n;
+
+	for (int i = 1; i<=n; ++i){
+		cin >> cnt[i];
+
+		psa[i] = psa[i-1] + cnt[i];
+	}
+
+	for (int i = 1; i<=n; ++i){
+		for (int j = 1; j + i <=n; ++j){
+			av = psa[i + j] - psa[j - 1];
+			//cout << "Stats: " << j << ' ' << av << '\n';
+
+
+			if (av % (i + 1) != 0){
+				//cout << "Skipped\n";
+				continue;
+			}
+
+			av /= i + 1;
+
+			for (int z = j; z <= i + j; ++z){
+				if (cnt[z] == av){
+					++ans;
+					break;
+				}
+			}
+		}
+	}
+
+	cout << ans << '\n';
+}
+//do not submit if usaco(); is open
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+	//Speed up
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+
+	//cin >> testcases; 
+	
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
+    
+		//Google
+		//cout<<"Case #"<<number_of_total_test_cases<<": ";
+
+
+		// Normal
+		solve();
+
+    }
+
+	//cout << elapsedTime << '\n';
+}
