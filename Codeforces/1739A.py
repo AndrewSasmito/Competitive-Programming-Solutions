f0acf261d01f3a4164045cc6cddb4072c7929d4d
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-10-01 13:22:02 -0400

    Update 1739A.py

--- Codeforces/1739A.py
+++ Codeforces/1739A.py
@@ -1,9 +1,4 @@
+import math
 for _ in range(int(input())):
     m = list(map(int, input().split()))
-    
-    if (m[0] % 2 == 1):
-        m[0] += 1
-    if (m[1] % 2 == 1):
-        m[1] += 1
-        
-    print (round(m[0] / 2), round(m[1] / 2))
+    print (math.ceil(m[0] / 2), math.ceil(m[1] / 2))
