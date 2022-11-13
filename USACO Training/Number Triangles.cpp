dcc40300d8471f43c49828286eaa337f9aeae721
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-11-12 23:02:58 -0500

    Update Number Triangles.cpp

--- USACO Training/Number Triangles.cpp
+++ USACO Training/Number Triangles.cpp
@@ -7,7 +7,6 @@ clock_t __init_time__ = clock();
 #define all(x) x.begin(), x.end()
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 char _;
-
 //custom hash for hash map
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
@@ -22,69 +21,66 @@ struct custom_hash {
         static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + fixed_random);
     }
+
+	/*
+	 	* How To Use:
+	 	*
+	 	* unordered_map<long long, int, custom_hash> safe_map;
+	 	* gp_hash_table<long long, int, custom_hash> safe_hash_table;
+	 	*
+	 */
 };
 
-//unordered_map<long long, int, custom_hash> safe_map;
-//gp_hash_table<long long, int, custom_hash> safe_hash_table;
+
 
 void usaco(){
+
 	freopen("numtri.in", "r", stdin);
 	freopen("numtri.out", "w", stdout);
+
+	/*
+		 * How To Use:
+		 *
+		 * Call usaco() to take input and output
+		 * to & from a textfile
+		 *
+		 * freopen("File Name", "r" or "w", stdin or stdout);
+		 *
+		 * r && stdin == read
+		 *
+		 * w && stdout == print
+		 *
+	 */
 }
 
-int mx = 0, n, x, cnt = 0;
-vector <int> v[1001];
-int dp[1001][1001];
+int n, ans = 0, grid[1001][1001], dp[1001][1001];
 void solve(){
 	usaco();
 	cin >> n;
-	cnt = 0;
-	for (int i = 0; i<n; ++i){
 
-		for (int j = 0; j<i + 1; ++j){
-			cin >> x;
-			cnt += x;
-			v[i].push_back(x);
+	for (int i = 1; i<=n; ++i){
+		for (int j = 1; j<=i; ++j){
+			cin >> grid[i][j];
 		}
 	}
-	if (cnt == 0){
-		cout << 0 << '\n';
-		return;
-	}else if (cnt == 1){
-		cout << 1 << '\n';
-		return;
-	}
-
-
-	queue <pair<int, int>> q;
-
-	q.push({0, 0});
-
-	while (q.size()){
-		int i = q.front().first;
-		int pos = q.front().second;
-		q.pop();
-
-		if (i == n - 1){
-			mx = max(mx, dp[i][pos]);
-			continue;
-		}
-
-		if (dp[i][pos] + v[i + 1][pos] > dp[i+1][pos]){
-			dp[i+1][pos] = dp[i][pos] + v[i + 1][pos];
-			q.push({i + 1, pos});
-		}
-
-		if (dp[i][pos] + v[i + 1][pos + 1] > dp[i+1][pos+1]){
-			dp[i+1][pos + 1] = dp[i][pos] + v[i + 1][pos + 1];
-			q.push({i + 1, pos + 1});
-
+	dp[1][1] = grid[1][1];
+	for (int i = 2; i<=n; ++i){
+		dp[i][1] = dp[i - 1][1] + grid[i][1];
+		dp[i][i] = dp[i - 1][i - 1] + grid[i][i];
+		for (int j = 2; j<i; ++j){
+			dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]);
+			dp[i][j] += grid[i][j];
 		}
 	}
-	cout << mx + v[0][0] << '\n';
+
+	for (int i = 1; i<=n; ++i){
+		ans = max(ans, dp[n][i]);
+	}
+	cout << ans << '\n';
 }
 
 //do not submit if usaco(); is open
+
 
 /* stuff you should look for
 	* int overflow, array bounds
@@ -93,21 +89,26 @@ void solve(){
 	* WRITE STUFF DOWN
 	* DON'T GET STUCK ON ONE APPROACH
 */
+
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	//Speed up
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 
     int testcases = 1;
+
 	//cin >> testcases; 
-	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){ 
-    
 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){
 
-    //Google
-    //cout<<"Case #"<<number_of_total_test_cases<<": ";
-    // Normal
-    solve();
+		//Google
+		//cout<<"Case #"<<number_of_total_test_cases<<": ";
+
+		// Normal
+		solve();
+
     }
 
 	//cout << elapsedTime << '\n';
 }
+
