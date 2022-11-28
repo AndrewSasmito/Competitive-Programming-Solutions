ec7bed923fc04b05335972783509de565886d437
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-11-27 23:17:25 -0500

    Create GlobeX Cup '18 S Sample - Chair Stacking.cpp

---
+++ Uncategorized - Dmoj/GlobeX Cup '18 S Sample - Chair Stacking.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+
+	/*
+	 	* How To Use:
+	 	*
+	 	* unordered_map<long long, int, custom_hash> safe_map;
+	 	* gp_hash_table<long long, int, custom_hash> safe_hash_table;
+	 	*
+	 */
+};
+
+
+
+void usaco(){
+
+	freopen("numtri.in", "r", stdin);
+	freopen("numtri.out", "w", stdout);
+
+	/*
+		 * How To Use:
+		 *
+		 * Call usaco() to take input and output
+		 * to & from a textfile
+		 *
+		 * freopen("File Name", "r" or "w", stdin or stdout);
+		 *
+		 * r && stdin == read
+		 *
+		 * w && stdout == print
+		 *
+	 */
+}
+
+int n;
+ll ans, mnx = 100000000000, mxx = 0, mny = 100000000000, mxy = 0, x, y, prt;
+void solve(){
+  //Double binary search, there was a greedy?
+	cin >> n;
+	vector <pair<ll, ll>> v;
+	for (int i = 0; i<n; ++i){
+		cin >> x >> y;
+
+		v.push_back(make_pair(x, y));
+		mnx = min(mnx, x), mxx = max(mxx, x);
+		mny = min(mny, y), mxy = max(mxy, y);
+	}
+	
+	ll lo = mnx, hi = mxx;
+	while (lo < hi){
+		ll cur = (lo + hi) / 2, f = 0, s = 0;
+		for (int i = 0; i<n; ++i){
+			f += abs(cur - v[i].first);
+			s += abs((cur + 1) - v[i].first);
+		}
+
+		if (f < s){
+			ans = f;
+			hi = cur;
+		}else{
+			ans = s;
+			lo = cur + 1;
+		}
+	}
+	
+	lo = mny, hi = mxy, prt += ans, ans = 0;
+	while (lo < hi){
+		ll cur = (lo + hi) / 2, f = 0, s = 0;
+		for (int i = 0; i<n; ++i){
+			f += abs(cur - v[i].second);
+			s += abs((cur + 1) - v[i].second);
+		}
+
+		if (f < s){
+			ans = f;
+			hi = cur;
+		}else{
+			ans = s;
+			lo = cur + 1;
+		}
+	}
+	cout << ans + prt << '\n';
+}
+
+//do not submit if usaco(); is open
+
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+	//Speed up
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int testcases = 1;
+
+	//cin >> testcases; 
+
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){
+
+		//Google
+		//cout<<"Case #"<<number_of_total_test_cases<<": ";
+
+		// Normal
+		solve();
+
+    }
+
+	//cout << elapsedTime << '\n';
+}
