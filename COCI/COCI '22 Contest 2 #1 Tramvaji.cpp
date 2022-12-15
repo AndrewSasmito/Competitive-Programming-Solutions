9d42523166864fa222885f518464719164fced6a
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-12-14 21:58:37 -0500

    Create  COCI '22 Contest 2 #1 Tramvaji.cpp

---
+++ COCI/COCI '22 Contest 2 #1 Tramvaji.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+
+	/*
+	 	* How To Use:
+	 	*
+	 	* unordered_map<long long, int, custom_hash> safe_map;
+	 	* gp_hash_table<long long, int, custom_hash> safe_hash_table;
+	 	*
+	 */
+};
+
+//Max and Min long long size
+const ll mxll = 9223372036854775807, mnll = -9223372036854775807;
+//Max and min int size
+const int mxint = 2147483647, mnint = -2147483647;
+
+void usaco(){
+
+	freopen("numtri.in", "r", stdin);
+	freopen("numtri.out", "w", stdout);
+
+	/*
+		 * How To Use:
+		 *
+		 * Call usaco() to take input and output
+		 * to & from a textfile
+		 *
+		 * freopen("File Name", "r" or "w", stdin or stdout);
+		 *
+		 * r && stdin == read
+		 *
+		 * w && stdout == print
+		 *
+	 */
+}
+
+int n, start, ender;
+ll dp[1002], ans = 99999999999999, dur, stat;
+string s;
+void solve(){
+	cin >> n;
+	vector <pair<ll, int>> v;
+	v.push_back(make_pair(0, 1));
+	for (int i = 2; i<=n; ++i){
+		cin >> s;
+
+		if (s == "Patrik"){
+			cin >> dur;
+			v.push_back({dur, i});
+			dp[i] = v.back().first;
+		}else{
+			cin >> stat >> dur;
+
+			v.push_back(make_pair(dp[stat] + dur, i));
+			dp[i] = v.back().first;
+		}
+	}
+	sort(v.begin(), v.end());
+	for (int i = 1; i<v.size(); ++i){
+		//cout << v[i].first << ' ' << v[i].second << ' ' << v[i - 1].first << ' ' << v[i - 1].second << '\n';
+		if (v[i].first - v[i - 1].first < ans){
+			ans = v[i].first - v[i - 1].first;
+			start = v[i - 1].second, ender = v[i].second;
+		}
+	}
+	cout << ans << ' ' << start << ' ' << ender << '\n';
+}
+
+//do not submit if usaco(); is open
+
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+	//Speed up
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
+    int testcases = 1;
+
+	//cin >> testcases; 
+
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){
+
+		//Google
+		//cout<<"Case #"<<number_of_total_test_cases<<": ";
+
+		// Normal
+		solve();
+
+    }
+
+	//cout << elapsedTime << '\n';
+}
