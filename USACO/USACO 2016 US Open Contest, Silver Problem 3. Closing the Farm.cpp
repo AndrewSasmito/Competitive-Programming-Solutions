73f93d5c13637b097160965b9fb813310466773c
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2022-12-27 20:53:41 -0500

    Create USACO 2016 US Open Contest, Silver Problem 3. Closing the Farm.cpp

---
+++ USACO/USACO 2016 US Open Contest, Silver Problem 3. Closing the Farm.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+
+	/*
+	 	* How To Use:
+	 	*
+	 	* unordered_map<long long, int, custom_hash> safe_map;
+	 	* gp_hash_table<long long, int, custom_hash> safe_hash_table;
+	 	*
+	 */
+};
+
+//Max and Min long long size
+const ll mxll = 9223372036854775807, mnll = -9223372036854775807;
+//Max and min int size
+const int mxint = 2147483647, mnint = -2147483647;
+
+void usaco(){
+
+	freopen("closing.in", "r", stdin);
+	freopen("closing.out", "w", stdout);
+
+	/*
+		 * How To Use:
+		 *
+		 * Call usaco() to take input and output
+		 * to & from a textfile
+		 *
+		 * freopen("File Name", "r" or "w", stdin or stdout);
+		 *
+		 * r && stdin == read
+		 *
+		 * w && stdout == print
+		 *
+	 */
+}
+const int MX = 1e5 + 1;
+int n, m, strt, x, y;
+bool f;
+bool vis[MX], opn[MX];
+vector <int> adj[MX];
+
+void calc(int x){
+	vis[x] = 1;
+	for (int z : adj[x]){
+		if (vis[z] == 1)
+			continue;
+
+		calc(z);
+	}
+}
+
+void solve(){
+	usaco();
+	cin >> n >> m;
+
+	for (int i = 0; i<m; ++i){
+		cin >> x >> y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+
+
+	for (int i = 0; i<n; ++i){
+		cin >> x;
+		
+		for (int j = 1; j<=n; ++j){
+			vis[j] = opn[j];
+			if (opn[j] == 0)
+				strt = j;
+		}
+
+		calc(strt);
+		f = 0;
+
+		for (int j = 1; j<=n; ++j){
+			if (vis[j] == 0)
+				f = 1;
+		}
+
+		cout << (f == 0 ? "YES" : "NO") << '\n';
+
+		opn[x] = 1;
+	}
+}
+
+//do not submit if usaco(); is open
+
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+int main(){
+	//Speed up
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+    int testcases = 1;
+
+	//cin >> testcases; 
+
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){
+
+		//Google
+		//cout<<"Case #"<<number_of_total_test_cases<<": ";
+
+		// Normal
+		solve();
+
+    }
+
+	//cout << elapsedTime << '\n';
+}
