83dfd06ab3add5f2b9df4e306e63d5991ac2f623
AndrewSasmito <98786007+AndrewSasmito@users.noreply.github.com>
2023-02-20 00:20:23 -0500

    Create Range Xor Queries.cpp

---
+++ CSES Problem Set/Range Xor Queries.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+//runtime
+clock_t __init_time__ = clock();
+#define elapsedtime ((double)(clock()-__init_time__)/clocks_per_sec)<<" seconds"
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+//custom hash for hash map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t fixed_random = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + fixed_random);
+    }
+ 
+	/*
+	 	* How To Use:
+	 	*
+	 	* unordered_map<long long, int, custom_hash> safe_map;
+	 	* gp_hash_table<long long, int, custom_hash> safe_hash_table;
+	 	*
+	 */
+};
+ 
+//Max and Min long long size
+const ll mxll = 9223372036854775807, mnll = -9223372036854775807;
+//Max and min int size
+const int mxint = 2147483647, mnint = -2147483647;
+ 
+void usaco(){
+ 
+	freopen("diamond.in", "r", stdin);
+	freopen("diamond.out", "w", stdout);
+ 
+	/*
+		 * How To Use:
+		 *
+		 * Call usaco() to take input and output
+		 * to & from a textfile
+		 *
+		 * freopen("File Name", "r" or "w", stdin or stdout);
+		 *
+		 * r && stdin == read
+		 *
+		 * w && stdout == print
+		 *
+	 */
+}
+
+int n, q, dp[200001];
+void solve(){
+	cin >> n >> q;
+	for (int i = 1; i<=n; ++i){
+		cin >> dp[i];
+		dp[i] ^= dp[i - 1];
+	}
+	for (int i = 0, x, y; i<q; ++i){
+		cin >> x >> y;
+		cout << (dp[y]^dp[x - 1]) << '\n';
+	}
+}
+
+//do not submit if usaco(); is open
+ 
+ 
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+ 
+int main(){
+	//Speed up
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+    int testcases = 1;
+	//cin >> testcases; 
+	for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){
+ 
+		//Google
+		//cout<<"Case #"<<number_of_total_test_cases<<": ";
+ 
+		// Normal
+		solve();
+ 
+    }
+ 
+	//cout << elapsedTime << '\n';
+}
